2ed013af93471b0044a05a8879cf2742a6c87e1c
Michael <michael.bostwick@gmail.com>
2017-05-15 10:09:43 -0400

    Add comments

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,8 @@
 import pyodbc, csv
+# pyodbc documentation available at: https://github.com/mkleehammer/pyodbc/wiki
 
 
+# Create connection to access file
 def open_mdb(month, mon, day):
     drv = 'Microsoft Access Driver (*.mdb, *.accdb)'
     if day < 10:
@@ -11,15 +13,18 @@ def open_mdb(month, mon, day):
     cnxn = pyodbc.connect('DRIVER={};DBQ={}'.format(drv, mdb))
     return cnxn.cursor()
 
+# Initialize master files
 def create_masters(col_names, table_type):
     with open('data/{}/Master_{}.csv'.format(table_type, table_type), 'w', newline = '') as fou:
         csv_writer = csv.writer(fou) 
         csv_writer.writerows([col_names])
 
+# Read in table from access file and write to csv
 def read_write(table_type, date):
     SQL = 'SELECT * FROM {};'.format(table_type) 
     rows = crsr.execute(SQL).fetchall()
 
+	# Only create master file once
     if date == 'D01012008':
         create_masters(header[table_type], table_type)
 
@@ -36,7 +41,7 @@ def read_write(table_type, date):
         csv_writer = csv.writer(fou) 
         csv_writer.writerows(rows)
         
-
+# Extract column names for each table type and save in dictionary
 def col_dict(tables):    
     header_dict = {}
     crsr = open_mdb('January2008', 1, 1)
@@ -48,16 +53,15 @@ def col_dict(tables):
         header_dict[table_type] = names
     return header_dict 
 
+# Define months, days and table types to iterate through
 months = ['January2008', 'February2008', 'March2008']
 days = [31, 29, 31]
-#months = ['January2008']
-#days = [3]
-
 tables = ['agent_events', 'agent_profile', 'agent_records', 'agent_shifts',
           'calls', 'cust_subcalls', 'event_details', 'server_subcalls']
 
 header = col_dict(tables)
 
+# Main loop to iterate through all tables and write to csv
 for i, month in enumerate(months):
     for day in range(1,days[i]+1):
         print(month, "Day:", day)
